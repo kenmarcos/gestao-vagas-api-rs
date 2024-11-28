52436dab494bb73eb812b15ddd3d5e53446123ec
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-11-28 11:56:21 -0300

    Criando repositório

--- src/main/java/br/com/kenmarcos/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/kenmarcos/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,25 @@
 package br.com.kenmarcos.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
 
@@ -25,4 +34,6 @@ public class CandidateEntity {
   private String description;
   private String curriculum;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
